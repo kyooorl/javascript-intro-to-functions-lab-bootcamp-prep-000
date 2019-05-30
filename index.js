9f15f78ee82518aeedbbb1560a3e4f4d0badb394
Monica Lee <mylee.715@gmail.com>
2019-05-30 17:40:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toUpperCase === string;
-  return "I can`t hear you!";
+  if string.toUpperCase === string {
+    return return "I can`t hear you!";
   
 }
